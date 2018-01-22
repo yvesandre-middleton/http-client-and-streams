742bf725d76a681c2041b9813516757e1d98c940
yvesandre-middleton <yvesmiddleton@gmail.com>
2018-01-22 00:14:33 +0000

    Incoming Message !

--- https_example.js
+++ https_example.js
@@ -11,6 +11,11 @@ var options = {
 var callback = function() {
   console.log('In response handler callback!');
 }
+// called by https when the request is made.
+var callback = function(response) {
+  console.log('In response handler callback!');
+  console.log('Response: ', response);
+}
 
 console.log("I'm about to make the request!");
 
